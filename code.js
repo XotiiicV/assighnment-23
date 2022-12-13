784795a23a307ac689deaa1fb48ebf7b3a2fa897
XotiiicV <116581229+XotiiicV@users.noreply.github.com>
2022-12-13 10:35:09 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,10 @@
+function hello(name) {
+    alert("hello " + name);
+}
+
+function secretMessage(message) {
+    console.log("The secret message is " + message);
+}
+
+hello("Can you find the secret message");
+secretMessage("I like potatoes!");
